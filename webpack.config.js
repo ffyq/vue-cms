ce32850c29fbdaef3cef52cae608d17b5c54fd42
fyq <819384417@qq.com>
2019-01-12 15:24:57 +0800

    项目完成打包

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=7631' },
+      { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader' },
       { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' },
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.vue$/, use: 'vue-loader' },
